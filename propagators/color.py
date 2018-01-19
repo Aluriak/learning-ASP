2ebf2676be6f230895e6b01d91d91936853ae88f
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2018-01-19 14:35:09 +0100

    prop:color: better handling of amount of data per line using textwrap

--- propagators/color.py
+++ propagators/color.py
@@ -10,48 +10,57 @@ Blessing would have been used for terminal cleaning if it worked.
 
 
 import os
+import shutil
+import textwrap
 from time import sleep
+from collections import defaultdict
 import colorama  # term colors
 from colorama import Fore, Back
 colorama.init()  # for windows
 
 
-SPEED = 0.3
-ATOM_PER_LINE = 14
+SPEED = 0.5
+TERM_WIDTH = shutil.get_terminal_size().columns
 def clear(): os.system('cls' if os.name == 'nt' else 'clear')
 
 
 class ColorPropagator:
     def init(self, init):
         self.sleep = SPEED
-        self.__symbols = {}
+        self.__symbols = defaultdict(set)
         for atom in init.symbolic_atoms:
-            init.add_watch(init.solver_literal(atom.literal))
-            self.__symbols[init.solver_literal(atom.literal)] = atom.symbol
+            lit = init.solver_literal(atom.literal)
+            init.add_watch(lit)
+            self.__symbols[lit].add(atom.symbol)
 
     def propagate(self, ctl, changes):
+        # print('PROPAGATE:', changes)
         self.print_atoms(ctl.assignment, changes=changes)
 
     def undo(self, solver_id, assign, undo):
+        # print('     UNDO:', undo)
         self.print_atoms(assign, changes=undo)
 
     def print_atoms(self, assignment, changes):
         clear()
         changes = frozenset(changes)  # speedup lookup
         assert all(change in self.__symbols for change in changes)
-        for idx, (lit, atom) in enumerate(self.__symbols.items(), start=1):
+        to_print = ''
+        for idx, (lit, atoms) in enumerate(self.__symbols.items(), start=1):
+            atom = ';'.join(map(str, atoms))
             assert assignment.has_literal(lit)
+            # help(assignment)
+            fore, back = Fore.WHITE, Back.BLACK
             if assignment.value(lit) is not None:
-                color = Fore.GREEN if assignment.is_true(lit) else Fore.RED
-                color += (Back.WHITE if lit in changes else '')
-                print(color + str(atom) + Fore.RESET + Back.RESET, end='  ')
+                assert assignment.is_true(lit) or assignment.is_false(lit)
+                fore = Fore.GREEN if assignment.is_true(lit) else Fore.RED
+                if lit in changes: back = Back.WHITE
             elif lit in changes:  # in case of undo: not assigned by in changes
-                print(Back.BLUE + str(atom) + Back.RESET, end='  ')
+                back = Back.BLUE
             else:
-                print(str(atom), end='  ')
-            if idx % ATOM_PER_LINE == 0:
-                print()
-        print('\n\n', flush=True)
+                pass
+            to_print += fore + back + atom + Fore.RESET + Back.RESET + '  '
+        print('\n\n' + '\n'.join(textwrap.wrap(to_print, width=TERM_WIDTH)))
         sleep(self.sleep)
 
 
